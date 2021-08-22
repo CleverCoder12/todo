15812f9c10c663289d21050fab0863425c88a39f
Tino <anthony1@gmail.com>
2021-08-22 15:45:21 -0500

    todo

--- src/App.js
+++ src/App.js
@@ -1,56 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todo, setTodo] = useState(["country", "state", "parish"]);
+  const [input, setInput] = useState(" ");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (input.length === 1) {
+      return null;
+    } else {
+      setTodo([...todo, input]);
+      setInput(" ");
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <h1>Todo list</h1>
+
+      <div className="group">
+        <form>
+          <input
+            className="in"
+            placeholder="todo list"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button type="submit" onClick={handleSubmit}>
+            Send
+          </button>
+        </form>
+      </div>
+      {todo.map((item) => (
+        <li className="list">{item}</li>
+      ))}
     </div>
   );
 }
